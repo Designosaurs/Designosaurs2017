2f6a53515364397a860a43019d8c1f3504f50a7c
William Teder <wteder@hydreon.com>
2016-11-15 18:34:24 -0600

    Better logging

--- TeamCode/src/main/java/org/designosaurs/DesignosaursAuto.java
+++ TeamCode/src/main/java/org/designosaurs/DesignosaursAuto.java
@@ -1,17 +1,32 @@
 package org.designosaurs;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.Log;
 
 import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.Matrix34F;
+import com.vuforia.Tool;
+import com.vuforia.Vec3F;
 
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+
+import java.util.Arrays;
 
 import ftc.vision.BeaconColorResult;
 import ftc.vision.BeaconProcessor;
-import org.opencv.android.OpenCVLoader;
 
 @Autonomous(name = "Designosaurs Autonomous", group = "Auto")
 public class DesignosaursAuto extends LinearOpMode {
@@ -25,33 +40,57 @@ public class DesignosaursAuto extends LinearOpMode {
 
 	private final String VUFORIA_LICENCE_KEY = "ATwI0oz/////AAAAGe9HyiYVEU6pmTFAb65tOfUrioTxlZtITHRLN1h3wllaw67kJsUOHwPVDsCN0vxiKy/9Qi9NnjpkVfUnn0gwIHyKJgTYkG7+dCaJtFJlY94qa1YPCy0y4rwhVQFkDkcaCiNoiS7ZSU5KLeIABF4Gvz9qYwJJtwxWGp4fbjyu+arTOUw160+Fg5XMjoftS8FAQPx4wF33sVdGw+CYX0fHdwQzOyN0PpIwBQ9xvb8e1c76FoHF0YUZyV/q0XeR97nRj1TfnesPc+v7Z72SEDCXAAdVVS6L9u/mVAxq4zTaXsdGcVsqHeaouoGmQ/1Ey/YYShqHaRZXWwC4GsgaxO9tCkWNH+hTjFZA2pgvKVl5HmLR";
 
+	private static final double FAST_DRIVE_POWER = 1.0;
+	private static final double TURN_POWER = 0.4;
 	private static final double DRIVE_POWER = 0.2;
 	private static final double SLOW_DOWN_AT = 3000;
 	private static final int BEACON_ALIGNMENT_TOLERANCE = 100;
 
 	/* State Machine Options */
-	private final byte STATE_SEARCHING = 0;
-	private final byte STATE_ALIGNING_WITH_BEACON = 1;
-	private final byte STATE_WAITING_FOR_PLACER = 2;
-	private final byte STATE_ROTATING_TOWARDS_GOAL = 3;
-	private final byte STATE_DRIVING_TOWARDS_GOAL = 4;
-	private final byte STATE_SCORING_IN_GOAL = 5;
-	private final byte STATE_DRIVING_TO_RAMP = 6;
-	private final byte STATE_FINISHED = 7;
+	private final byte STATE_INITIAL_POSITIONING = 0;
+	private final byte STATE_SEARCHING = 1;
+	private final byte STATE_ALIGNING_WITH_BEACON = 2;
+	private final byte STATE_WAITING_FOR_PLACER = 3;
+	private final byte STATE_ROTATING_TOWARDS_GOAL = 4;
+	private final byte STATE_DRIVING_TOWARDS_GOAL = 5;
+	private final byte STATE_SCORING_IN_GOAL = 6;
+	private final byte STATE_DRIVING_TO_RAMP = 7;
+	private final byte STATE_FINISHED = 8;
 
 	/* Current State */
-	private byte autonomousState = STATE_SEARCHING;
+	private byte autonomousState = STATE_INITIAL_POSITIONING;
 	private int ticksInState = 0;
 	private BeaconColorResult lastBeaconColor;
 	private BeaconColorResult.BeaconColor targetColor;
+	private String stateMessage = "Starting...";
 
 	private static String TAG = "DesignosaursAuto";
+
+	private void setInitState(String state) {
+		telemetry.clear();
+		telemetry.addLine("== Designosaurs 2017 ==");
+		telemetry.addLine(state);
+		telemetry.update();
+	}
+
+	public void updateRunningState() {
+		telemetry.clear();
+		telemetry.addLine(stateMessage);
+		telemetry.addLine("");
+		telemetry.addLine("State: " + String.valueOf(autonomousState));
+		telemetry.addLine("Ticks in state: " + String.valueOf(ticksInState));
+		telemetry.update();
+	}
 
 	@Override
 	public void runOpMode() throws InterruptedException {
 		boolean isFirstRun = true;
 
+		setInitState("Configuring hardware...");
+
 		robot.init(hardwareMap);
+
+		setInitState("Initializing vuforia...");
 
 		VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
 		params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
@@ -65,15 +104,15 @@ public class DesignosaursAuto extends LinearOpMode {
 		beacons.get(2).setName("legos");
 		beacons.get(3).setName("gears");
 
+		setInitState("Initializing OpenCV...");
+
 		OpenCVLoader.initDebug();
+
+		setInitState("Ready!");
 
 		waitForStart();
 
-		robot.turn(90, 0.4);
-
-		/*
-
-		buttonPusherManager.setState(buttonPusherManager.STATE_HOMING);
+		buttonPusherManager.start();
 		beacons.activate();
 
 		byte beaconsFound = 0;
@@ -138,23 +177,38 @@ public class DesignosaursAuto extends LinearOpMode {
 			if(isFirstRun)
 				centeredPos = Integer.MAX_VALUE;
 
-			telemetry.addData("state", autonomousState);
-			telemetry.update();
-
 			buttonPusherManager.update();
 
 			switch(autonomousState) {
+				case STATE_INITIAL_POSITIONING:
+					stateMessage = "Getting in position...";
+
+					updateRunningState();
+
+					robot.goStraight(5, FAST_DRIVE_POWER);
+					robot.turn(45, TURN_POWER);
+					robot.goStraight(0.5, FAST_DRIVE_POWER);
+
+					setState(STATE_SEARCHING);
+				break;
 				case STATE_SEARCHING:
 					if(ticksInState > 1000) {
-						Log.i("relativePosition", String.valueOf(getRelativePosition()));
+						stateMessage = "Searching for beacon...";
 
-						if(Math.abs(getRelativePosition()) > SLOW_DOWN_AT)
+						if(Math.abs(getRelativePosition()) > SLOW_DOWN_AT)  {
+							stateMessage = "Slowing down and searching for beacon.";
+
 							robot.setDrivePower(0.6 * DRIVE_POWER);
+						}
 
 						if(Math.abs(getRelativePosition()) < BEACON_ALIGNMENT_TOLERANCE) {
-							robot.setDrivePower(0);
+							stateMessage = "Analysing beacon data...";
+
+							robot.setDrivePower(0.08);
 
 							if(havePixelData) {
+								stateMessage = "Beacon found!";
+
 								lastBeaconColor = beaconProcessor.process(System.currentTimeMillis(), output, false).getResult();
 								targetColor = (imageName.equals("wheels") || imageName.equals("legos")) ? BeaconColorResult.BeaconColor.BLUE : BeaconColorResult.BeaconColor.RED;
 
@@ -168,15 +222,20 @@ public class DesignosaursAuto extends LinearOpMode {
 								setState(STATE_ALIGNING_WITH_BEACON);
 							} else
 								Log.w("DesignosaursAuto", "Beacon is seen, but failed to transfer data to OpenCV.");
-						} else
+						} else {
+							stateMessage = "Aligning with beacon...";
+
 							if(Math.abs(getRelativePosition()) < SLOW_DOWN_AT)
 								if(getRelativePosition() > 0)
 									robot.setDrivePower(DRIVE_POWER * 0.5);
 								else
 									robot.setDrivePower(DRIVE_POWER * -0.5);
+						}
 					}
 				break;
 				case STATE_ALIGNING_WITH_BEACON:
+					stateMessage = "Positioning to deploy placer...";
+
 					if(Math.abs(robot.getAdjustedEncoderPosition(robot.leftMotor)) >= 600) {
 						Log.i("DesignosaursAuto", "//// DEPLOYING ////");
 
@@ -190,6 +249,8 @@ public class DesignosaursAuto extends LinearOpMode {
 					}
 				break;
 				case STATE_WAITING_FOR_PLACER:
+					stateMessage = "Waiting for placer to deploy.";
+
 					if(buttonPusherManager.getState() != buttonPusherManager.STATE_SCORING) {
 
 						beaconsFound++;
@@ -201,11 +262,11 @@ public class DesignosaursAuto extends LinearOpMode {
 					}
 				break;
 				case STATE_ROTATING_TOWARDS_GOAL:
-					robot.rotateToPosition(45, DRIVE_POWER);
+					robot.rotateToPosition(45, TURN_POWER);
 					setState(STATE_DRIVING_TOWARDS_GOAL);
 				break;
 				case STATE_DRIVING_TOWARDS_GOAL:
-					robot.driveStraightFeet(4, DRIVE_POWER);
+					robot.goStraight(4, FAST_DRIVE_POWER);
 					setState(STATE_SCORING_IN_GOAL);
 				break;
 				case STATE_SCORING_IN_GOAL:
@@ -213,15 +274,14 @@ public class DesignosaursAuto extends LinearOpMode {
 					setState(STATE_DRIVING_TO_RAMP);
 				break;
 				case STATE_DRIVING_TO_RAMP:
-					robot.driveStraightFeet(3, DRIVE_POWER);
+					robot.goStraight(3, FAST_DRIVE_POWER);
 
 					setState(STATE_FINISHED);
 			}
 
 			ticksInState++;
+			updateRunningState();
 		}
-
-		*/
 	}
 
 	private void setState(byte newState) {
