0de62133716e19cec000d090c3b62eafb637ca2e
William Teder <wteder@hydreon.com>
2016-11-19 17:47:15 -0600

    Button pusher control in TeleOp

--- TeamCode/src/main/java/org/designosaurs/DesignosaursTeleOp.java
+++ TeamCode/src/main/java/org/designosaurs/DesignosaursTeleOp.java
@@ -8,17 +8,17 @@ public class DesignosaursTeleOp extends LinearOpMode {
 	private DesignosaursHardware robot = new DesignosaursHardware();
 
 	private static final double JOYSTICK_DEADBAND = 0.2;
-	private static final double DRIVE_POWER = 0.2;
+	private static final double DRIVE_POWER = 1.0;
+	private static final double BUTTON_PUSHER_POWER = 0.5;
 
 	@Override
 	public void runOpMode() throws InterruptedException {
-		double left;
-		double right;
+		double left, right, buttonPusher;
 
 		robot.init(hardwareMap);
 
-		telemetry.addData("00", "Designosaurs 2017");
-		telemetry.addData("00", "Ready to start!");
+		telemetry.addLine("Designosaurs 2017");
+		telemetry.addLine("Ready to start!");
 		telemetry.update();
 
 		waitForStart();
@@ -26,6 +26,8 @@ public class DesignosaursTeleOp extends LinearOpMode {
 		while(opModeIsActive()) {
 			left = -gamepad1.left_stick_y;
 			right = -gamepad1.right_stick_y;
+			buttonPusher = -gamepad2.left_stick_y;
+
 
 			if(Math.abs(left) < JOYSTICK_DEADBAND)
 				left = 0;
@@ -33,9 +35,13 @@ public class DesignosaursTeleOp extends LinearOpMode {
 			if(Math.abs(right) < JOYSTICK_DEADBAND)
 				right = 0;
 
+			if(Math.abs(buttonPusher) < JOYSTICK_DEADBAND)
+				buttonPusher = 0;
+
 			if(DesignosaursHardware.hardwareEnabled) {
 				robot.leftMotor.setPower(left * DRIVE_POWER);
 				robot.rightMotor.setPower(right * DRIVE_POWER);
+				robot.buttonPusher.setPower(buttonPusher * BUTTON_PUSHER_POWER);
 
 				telemetry.addData("00", "L power:   " + String.valueOf(left));
 				telemetry.addData("01", "L encoder: " + String.valueOf(robot.leftMotor.getCurrentPosition()));
