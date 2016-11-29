f8616dae078daf78b4f984dadeb7786c29e82d52
William Teder <wteder@hydreon.com>
2016-11-28 18:24:32 -0600

    Test code for gyroscope integration

--- TeamCode/src/main/java/org/designosaurs/DesignosaursHardware.java
+++ TeamCode/src/main/java/org/designosaurs/DesignosaursHardware.java
@@ -12,6 +12,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import java.util.Arrays;
+
 class DesignosaursHardware implements SensorEventListener {
 	static final boolean hardwareEnabled = true;
 
@@ -27,16 +29,11 @@ class DesignosaursHardware implements SensorEventListener {
 	private SparseIntArray encoderOffsets = new SparseIntArray(3);
 
 	private SensorManager mSensorManager;
-	private Sensor accelerometer;
-	private Sensor magnetometer;
+	private Sensor gyroscope;
 
-	// Orientation values in radians:
-	private float azimuth = 0.0f;
-	private float pitch = 0.0f;
-	private float roll = 0.0f;
-
-	private float[] mGravity;
-	private float[] mGeomagnetic;
+	private static final float NS2S = 1.0f / 1000000000.0f;
+	private final float[] deltaRotationVector = new float[4];
+	private float lastSensorReading;
 
 	DesignosaursHardware() {}
 
@@ -67,11 +64,9 @@ class DesignosaursHardware implements SensorEventListener {
 		}
 
 		mSensorManager = (SensorManager) hwMap.appContext.getSystemService(Context.SENSOR_SERVICE);
-		accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-		magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+		gyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
 
-		mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
-		mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+		mSensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_UI);
 	}
 
 	void waitForTick(long periodMs) throws InterruptedException {
@@ -189,28 +184,48 @@ class DesignosaursHardware implements SensorEventListener {
 	/*** Sensors ***/
 
 	public void onSensorChanged(SensorEvent event) {
-		// we need both sensor values to calculate orientation
-		// only one value will have changed when this method called, we assume we can still use the other value.
-		if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-			mGravity = event.values;
+		// This timestep's delta rotation to be multiplied by the current rotation
+		// after computing it from the gyro sample data.
+		if(lastSensorReading != 0) {
+			final float dT = (event.timestamp - lastSensorReading) * NS2S;
+			// Axis of the rotation sample, not normalized yet.
+			float axisX = event.values[0];
+			float axisY = event.values[1];
+			float axisZ = event.values[2];
 
-		if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
-			mGeomagnetic = event.values;
+			// Calculate the angular speed of the sample
+			double omegaMagnitude = Math.sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
 
-		if(mGravity != null && mGeomagnetic != null) { // make sure we have both before calling getRotationMatrix
-			float R[] = new float[9];
-			float I[] = new float[9];
-			boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
-
-			if(success) {
-				float orientation[] = new float[3];
-				SensorManager.getOrientation(R, orientation);
-
-				azimuth = orientation[0]; // orientation contains: azimuth, pitch and roll
-				pitch = orientation[1];
-				roll = orientation[2];
+			// Normalize the rotation vector if it's big enough to get the axis
+			// (that is, EPSILON should represent your maximum allowable margin of error)
+			if omegaMagnitude > 1) {
+				axisX /= omegaMagnitude;
+				axisY /= omegaMagnitude;
+				axisZ /= omegaMagnitude;
 			}
+
+			// Integrate around this axis with the angular speed by the timestep
+			// in order to get a delta rotation from this sample over the timestep
+			// We will convert this axis-angle representation of the delta rotation
+			// into a quaternion before turning it into the rotation matrix.
+			double thetaOverTwo = omegaMagnitude * dT / 2.0f;
+			double sinThetaOverTwo = Math.sin(thetaOverTwo);
+			double cosThetaOverTwo = Math.cos(thetaOverTwo);
+
+			deltaRotationVector[0] = (float) sinThetaOverTwo * axisX;
+			deltaRotationVector[1] = (float) sinThetaOverTwo * axisY;
+			deltaRotationVector[2] = (float) sinThetaOverTwo * axisZ;
+			deltaRotationVector[3] = (float) cosThetaOverTwo;
 		}
+
+		lastSensorReading = event.timestamp;
+		float[] deltaRotationMatrix = new float[9];
+		SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+		// User code should concatenate the delta rotation we computed with the current rotation
+		// in order to get the updated rotation.
+
+		Log.i("DesignosaursAuto", Arrays.toString(deltaRotationMatrix));
+
 	}
 
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
